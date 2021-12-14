ac08e801510b5a0a67ae0935268e55811db263e4
Hannah Burdge <hburdge1@gmail.com>
2021-12-14 09:58:15 -0800

    commit tree 5092e77f0df7d1128384a2dbaae2a0f27b79a1a2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+const upperCaseCurrentUser = currentUser.toUpperCase() ;
+
+let excitedWelcomeMessage ='WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!'
+
+let shortGreeting = 'Welcome, ' + currentUser[0] + '!'
